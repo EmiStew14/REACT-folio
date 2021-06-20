029efb611c1cfc32f03abf1fb994f3b259b12f8b
Emily Stewart <stewartemilym2014@gmail.com>
2021-06-20 15:17:11 -0700

    Updates

---
+++ precache-manifest.a9db03eef97115803889e8fcb233118f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46692a327954327d72723ee0f67c670d",
+    "url": "/REACT-folio/index.html"
+  },
+  {
+    "revision": "d2d1d02502a1f6df3f5e",
+    "url": "/REACT-folio/static/css/main.145b4d65.chunk.css"
+  },
+  {
+    "revision": "933142fa2371004bf5c0",
+    "url": "/REACT-folio/static/js/2.f1be6a2a.chunk.js"
+  },
+  {
+    "revision": "d2d1d02502a1f6df3f5e",
+    "url": "/REACT-folio/static/js/main.23e85523.chunk.js"
+  },
+  {
+    "revision": "79f2a9259a85d1cf3768",
+    "url": "/REACT-folio/static/js/runtime~main.059acd1d.js"
+  },
+  {
+    "revision": "2199c80de34da37f5b6c602c71ce9642",
+    "url": "/REACT-folio/static/media/0.2199c80d.PNG"
+  },
+  {
+    "revision": "f90c35ae4d45a4c9b8fa237f08aa4f77",
+    "url": "/REACT-folio/static/media/1.f90c35ae.PNG"
+  },
+  {
+    "revision": "0fbe37409d62d64059e64b0ffcb68009",
+    "url": "/REACT-folio/static/media/10.0fbe3740.PNG"
+  },
+  {
+    "revision": "0c28ba76e979aee4e1a40e00a927b6c8",
+    "url": "/REACT-folio/static/media/2.0c28ba76.PNG"
+  },
+  {
+    "revision": "0b9b8f5138108f926dbb2ab442b690fe",
+    "url": "/REACT-folio/static/media/3.0b9b8f51.PNG"
+  },
+  {
+    "revision": "81754b8b6939de9df2c4b499cb323fc5",
+    "url": "/REACT-folio/static/media/4.81754b8b.PNG"
+  },
+  {
+    "revision": "1d095c55e5a8942a052fb1b9ca07e4e7",
+    "url": "/REACT-folio/static/media/5.1d095c55.PNG"
+  },
+  {
+    "revision": "010384f6296ff98a8c6c8574874517ee",
+    "url": "/REACT-folio/static/media/6.010384f6.PNG"
+  },
+  {
+    "revision": "4ea1759a53c64a322d210a44cceccf00",
+    "url": "/REACT-folio/static/media/7.4ea1759a.PNG"
+  },
+  {
+    "revision": "e72338fa4429a8da8a968f794600764e",
+    "url": "/REACT-folio/static/media/8.e72338fa.PNG"
+  },
+  {
+    "revision": "e4f19ed52e340d2678172a883b5f1ddb",
+    "url": "/REACT-folio/static/media/9.e4f19ed5.PNG"
+  },
+  {
+    "revision": "ff0ed89260b82c78c15b4f058ac188a7",
+    "url": "/REACT-folio/static/media/Selfie2.ff0ed892.jpg"
+  },
+  {
+    "revision": "5d29679bafe685de4ff2818639280bc3",
+    "url": "/REACT-folio/static/media/Space.5d29679b.PNG"
+  }
+]);
